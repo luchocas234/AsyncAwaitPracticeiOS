509503a6002d87f22062d994446f38be61586621
Luciano Castro <lfcastro2911@gmail.com>
2023-02-02 12:55:52 -0300

    Transforming in MVVM, added Webservice

--- GettingStartedAsyncAwait/ContentView.swift
+++ GettingStartedAsyncAwait/ContentView.swift
@@ -20,28 +20,20 @@ struct ContentView: View {
 
     @State private var currentDates: [CurrentDate] = []
     
-    private func getDate() async throws -> CurrentDate? {
-        
-        guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
-            fatalError("Url is incorrect!")
-        }
-        
-        let (data, _) = try await URLSession.shared.data(from: url)
-        return try? JSONDecoder().decode(CurrentDate.self, from: data)
-    }
     
-    private func populateDate() async {
-      
-        do{
-            guard let currentDate = try await getDate() else { return}
-            
-            self.currentDates.append(currentDate)
-            
-        } catch{
-            print (error)
-        }
-        
-    }
+    
+//    private func populateDate() async {
+//
+//        do{
+//            guard let currentDate = try await getDate() else { return}
+//
+//            self.currentDates.append(currentDate)
+//
+//        } catch{
+//            print (error)
+//        }
+//
+//    }
     
     
     var body: some View {
